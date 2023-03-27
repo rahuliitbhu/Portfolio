ed6425094c1e2d26d0eb3811ee21faa9cb4fea59
Rahul Kumar <rahuliitbhu18@gmail.com>
2023-03-27 21:11:21 +0530

    first commit

---
+++ src/components/EmailMe/Email.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Email.css";
+import { useGetContactsQuery } from "../../Api/api";
+import emailjs from "@emailjs/browser";
+
+const Email = (e) => {
+  const form = useRef();
+ 
+  const sendEmail = (e) => {
+    e.preventDefault();
+   
+   /* emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );*/
+    e.target.reset();
+  };
+  const [name,setName]=useState("")
+  const [email,setEmail]=useState("")
+  const [subject,setSubject]=useState("")
+  const [message,setMessage]=useState("")
+  const { data: contacts, isFetching } = useGetContactsQuery();
+
+  const [contactsDetails, setContactDetails] = useState(contacts);
+  // const img_300 = "http://127.0.0.1:8000";
+  useEffect(() => {
+    setContactDetails(contacts);
+  }, [contactsDetails, contacts]);
+  console.log(contactsDetails)
+ 
+  if (isFetching) return "loading";
+  return (
+    <>
+      <div className="reachme-container">
+        <div className="reachme-title2">
+          <h2>I Want To Hear From You</h2>
+
+          <h3>Contact Me</h3>
+        </div>
+        <div className="row">
+          <div className="col-md-5">
+            <div className="reachme-title">
+              <div className="row">
+                {contactsDetails &&
+                  contactsDetails.map((details) => (
+                    <div className="contact-info  " key={details.id}>
+                      <div className="contact-details">
+                        <i className={details.icon}></i>
+                        <div className="contact-mi">
+                          <h4 className="icon-name">{details.contact_name}:</h4>
+                          <p className="d-name">{details.contact_info}</p>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 email-me container">
+            <form
+              action=""
+              className="contact-form"
+              ref={form}
+              onSubmit={sendEmail}
+            >
+              <div className="row">
+                <div className="col-md-12 mb-3 hire-me-title"></div>
+                <div className="col-md-6 ">
+                  <input
+                    type="text"
+                    name="user_name"
+                    id=""
+                    value={name}
+                    onChange={(e)=>{setName(e.target.value)}}
+                    placeholder="Enter Your Name"
+                  />
+                </div>
+                <div className="col-md-6 ">
+                  <input
+                    type="email"
+                    name="user_email"
+                    id=""
+                    value={email}
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                    placeholder="Enter Your Email"
+                  />
+                </div>
+                <div className="col-md-12">
+                  <input
+                    type="text"
+                    name="subject"
+                    id=""
+                    value={subject}
+                    onChange={(e)=>{setSubject(e.target.value)}}
+                    placeholder="Enter Subject"
+                  />
+                </div>
+                <div className="col-md-12 mb-2">
+                  <textarea
+                    name="message"
+                    id=""
+                    cols="60"
+                    rows="8"
+                    value={message}
+                    onChange={(e)=>{setMessage(e.target.value)}}
+                    placeholder="Your Message"
+                  ></textarea>
+                  <button className="hire-btn" >
+                  
+                  { <a href={`https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=&su=${subject}&body=${`Name:${name}%0D%0AEmail:${email}%0D%0A${message}`}`}>Send Message</a> }
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Email;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,84 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Main from "./nav";
+import work from "../../images/work.png"
+const Navbar = () => {
+  
+  return (
+    <>
+      <Main />
+      <button type="button" className="mobile-nav-toggle d-lg-none">
+        <i className="icofont-navigation-menu"></i>
+      </button>
+      <header id="header" className="fixed-top ">
+        <div className="container-fluid  navbur">
+          <div className="navi">
+            <div className="col-xl-12 d-flex align-items-center lefty">
+              <h5 className="ld-flex mr-auto devman">
+                <img
+                  src="https://code.google.com/images/developers.png"
+                  alt=""
+                />
+                Developer   <span className="blink"> _</span>
+              </h5>
+              <nav className="nav-menu  mainMenu">
+                <ul>
+                  <li
+                    className="active"
+                    data-aos="fade-down"
+                    data-aos-duration="300"
+                  >
+                    <a href="#home"> Home</a>
+                  </li>
+                  <li data-aos="fade-down" data-aos-duration="600">
+                    <a href="#about">About</a>
+                  </li>
+                  <li data-aos="fade-down" data-aos-duration="900">
+                    <a href="#services">Knowledge</a>
+                  </li>
+                  <li data-aos="fade-down" data-aos-duration="1200">
+                    <a href="#skills">Skills</a>
+                  </li>
+
+                  <li data-aos="fade-down" data-aos-duration="1500">
+                    <a href="#work">My Work</a>
+                  </li>
+
+                  <li data-aos="fade-down" data-aos-duration="1800">
+                    <a href="#contact">Contact</a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="nav-social">
+               <a href="https://github.com/rahuliitbhu"> <i className="fa fa-github"></i></a>
+              </div>
+              <div className="left-btns">
+                <div class=" " id="theme-button2">
+                  <input id="toggle" class="toggle" type="checkbox"></input>
+                </div>
+             
+                <div>
+                <a href={process.env.REACT_APP_PROFILE}>  
+                <img
+                  className="round-image" 
+        src={process.env.REACT_APP_PROFILE}
+        alt="Logo"
+        
+      /></a>
+    
+                </div>
+                <div className=" " id="theme-button">
+                  <Link to="#" class="menuBtn">
+                    <span class="lines"></span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
