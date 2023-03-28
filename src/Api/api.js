f961a2fca42374743a0e4519438abc5cbe6ae432
Rahul Kumar <rahuliitbhu18@gmail.com>
2023-03-28 15:05:22 +0530

    first commit

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // api url
-const baseUrl = "http://127.0.0.1:8000/api";
+const baseUrl = "https://portfolio-api-tau-fawn.vercel.app/api";
 
 // Heroes' Section
 export const HomeDetails = createApi({
